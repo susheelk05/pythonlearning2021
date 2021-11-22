4e2a261ed4831df343ab1abc0ef281e3d8c7de88
Ramana <ramanagoud2000@gmail.com>
2021-11-22 23:16:14 +0530

    buses program changed

--- assignments/buses_required.py
+++ assignments/buses_required.py
@@ -1,1 +1,12 @@
-print("Hello world")
+#take input from user, no of people, buses, seats per bus
+
+people = int(input("People :"))
+buses = int(input("Buses :"))
+seats = int(input("Seats :"))
+
+if people <= buses*seats:
+    print("Buses Sufficient")
+else:
+    x = people - (buses*seats)
+    y = x/seats
+    print("More %s buses are required"%y)
